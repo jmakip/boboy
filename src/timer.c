fdffdadc50e7b15dcd6cca5ad1ecde1be15c4b5b
Jarno Mäkipää <jmakip87@gmail.com>
2023-12-23 10:15:29 +0200

    add some timer register definitions

--- src/timer.c
+++ src/timer.c
@@ -5,17 +5,17 @@
 #include "mem.h"
 #include "cpu.h"
 
-#define DIVIDER_COUNT 0xFF04
-#define TIM_TIMA      0xFF05
-#define TIM_TMA       0xFF06
-#define TIM_TAC       0xFF07
+#define DIVIDER_COUNT 0xFF04u
+#define TIM_TIMA      0xFF05u
+#define TIM_TMA       0xFF06u
+#define TIM_TAC       0xFF07u
 
-#define TIM_ENABLE   0x04
-#define TIM_DIV_MASK 0x03
-#define TIM_DIV16    0x01
-#define TIM_DIV64    0x02
-#define TIM_DIV256   0x03
-#define TIM_DIV1024  0x00
+#define TIM_ENABLE   0x04u
+#define TIM_DIV_MASK 0x03u
+#define TIM_DIV16    0x01u
+#define TIM_DIV64    0x02u
+#define TIM_DIV256   0x03u
+#define TIM_DIV1024  0x00u
 
 // Internal counters for timers
 // div and tim counters should be internally connected
@@ -27,23 +27,23 @@ struct tim_cnt {
     uint16_t tim_arr;
 };
 
-struct tim_cnt tim = {
-    .div_count = 0,
-    .tim_count = 0,
-    .tim_arr = 0,
-};
 
 // This could be improved to have less function calls to mem handler
 // by having event based change system for 0xFF0X peripheral addresses
 // now we poll 4 million times per second when timer is enabled...
 void timer_tick()
 {
+    static struct tim_cnt tim = {
+        .div_count = 0,
+        .tim_count = 0,
+        .tim_arr = 0,
+    };
     uint8_t tac = mem_read(TIM_TAC);
 
-    tim.div_count = (tim.div_count + 1) & 0x3FFF;
-    mem_write(DIVIDER_COUNT, (tim.div_count>>14)&0xFF);
+    tim.div_count = (tim.div_count + 1u) & 0x3FFFFFu;
+    mem_write(DIVIDER_COUNT, (uint8_t)((tim.div_count >> 14) & 0xFFu));
 
-    if (tac & TIM_ENABLE) {
+    if ((tac & TIM_ENABLE) != NULL) {
         switch (tac & TIM_DIV_MASK) {
         case TIM_DIV1024:
             tim.tim_arr = 1024;
@@ -57,10 +57,13 @@ void timer_tick()
         case TIM_DIV256:
             tim.tim_arr = 256;
             break;
+        default:
+            //dead code
+            break;
         }
         if (++tim.tim_count >= tim.tim_arr) {
             uint8_t tima = mem_read(TIM_TIMA);
-            if (tima == 0xFF) {
+            if (tima == 0xFFu) {
                 uint8_t tma = mem_read(TIM_TMA);
                 mem_write(TIM_TMA, ++tma);
                 irq_request(0x50);
